95ccd9e6e8d701e76f8619c6e2fd8d2a6f8397fc
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-03-26 11:26:36 +0100

    Finished argument handling

--- src/maths.py
+++ src/maths.py
@@ -1,1 +1,28 @@
 from .error_handling import *
+from .matrix_operations import *
+
+def compute_exp(args):
+    args.pop(0)
+    matrix = create_matrix(args)
+    for line in matrix:
+        print(line)
+
+def compute_cos(args):
+    args.pop(0)
+    matrix = create_matrix(args)
+    pass
+
+def compute_sin(args):
+    args.pop(0)
+    matrix = create_matrix(args)
+    pass
+
+def compute_cosh(args):
+    args.pop(0)
+    matrix = create_matrix(args)
+    pass
+
+def compute_sinh(args):
+    args.pop(0)
+    matrix = create_matrix(args)
+    pass
