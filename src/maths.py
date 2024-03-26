49297a00bb6004e1f539ee303e05c64a959cad72
Colin Heurteaux <colin.heurteaux@epitech.eu>
2024-03-26 15:04:36 +0100

    Done EXP function

--- src/maths.py
+++ src/maths.py
@@ -1,11 +1,13 @@
 from .error_handling import *
 from .matrix_operations import *
 
-def compute_exp(args):
-    args.pop(0)
-    matrix = create_matrix(args)
-    for line in matrix:
-        print(line)
+NB_ITER = 100
+
+def compute_exp(matrix):
+    result = get_identity_matrix(len(matrix))
+    for n in range(1, NB_ITER):
+        result = add_matrices(result, divide_matrix(power_matrix(matrix, n), math.factorial(n)))
+    return result
 
 def compute_cos(args):
     args.pop(0)
@@ -26,3 +28,22 @@ def compute_sinh(args):
     args.pop(0)
     matrix = create_matrix(args)
     pass
+
+def detect_function(args):
+    float_args = [float(args[i]) for i in range(2, len(args))]
+    base_matrix = create_matrix(float_args)
+    match args[1]:
+        case "EXP":
+            result = compute_exp(base_matrix)
+        case "COS":
+            result = compute_cos(base_matrix)
+        case "SIN":
+            result = compute_sin(base_matrix)
+        case "COSH":
+            result = compute_cosh(base_matrix)
+        case "SINH":
+            result = compute_sinh(base_matrix)
+        case _:
+            display_error("Unknown fun argument \"{}\", please refer to help for known fun.".format(args[0]))
+            exit(84)
+    display_matrix(result)
--- src/matrix_operations.py
+++ src/matrix_operations.py
@@ -1,8 +1,10 @@
 import math
 from .error_handling import *
 
+def get_identity_matrix(size):
+    return [[1 if i == j else 0 for j in range(size)] for i in range(size)]
+
 def create_matrix(args):
-    import math
     matrix_size = int(math.sqrt(len(args)))
     args_index = 0
     matrix = []
@@ -18,24 +20,34 @@ def create_matrix(args):
         matrix.append(line)
     return matrix
 
+def multiply_matrix_nb(matrix, nb):
+    result = []
+
+    for i in range(len(matrix)):
+        line = []
+        for j in range(len(matrix)):
+            line.append(matrix[i][j] * nb)
+        result.append(line)
+    return result
+
 def add_matrices(matrix_a, matrix_b):
     result = []
 
     for i in range(len(matrix_a)):
-        row = []
+        line = []
         for j in range(len(matrix_a[i])):
-            row.append(matrix_a[i][j] + matrix_b[i][j])
-        result.append(row)
+            line.append(matrix_a[i][j] + matrix_b[i][j])
+        result.append(line)
     return result
 
 def substract_matrices(matrix_a, matrix_b):
     result = []
 
     for i in range(len(matrix_a)):
-        row = []
+        line = []
         for j in range(len(matrix_a[i])):
-            row.append(matrix_a[i][j] - matrix_b[i][j])
-        result.append(row)
+            line.append(matrix_a[i][j] - matrix_b[i][j])
+        result.append(line)
     return result
 
 def multiply_matrices(matrix_a, matrix_b):
@@ -46,7 +58,30 @@ def multiply_matrices(matrix_a, matrix_b):
         for j in range(len(matrix_b[0])):
             value = 0
             for k in range(len(matrix_a[i])):
-                value += matrix_a[i][k] * matrix_b[k][j]
+                value += (matrix_a[i][k] * matrix_b[k][j])
             row.append(value)
         result.append(row)
     return result
+
+def power_matrix(matrix, power):
+    result = matrix
+
+    for i in range(power - 1):
+        result = multiply_matrices(result, matrix)
+    return result
+
+def divide_matrix(matrix, nb):
+    result = []
+
+    for i in range(len(matrix)):
+        line = []
+        for j in range(len(matrix[i])):
+            line.append(matrix[i][j] / nb)
+        result.append(line)
+    return result
+
+def display_matrix(matrix):
+    for line in matrix:
+        for i in range(len(matrix) - 1):
+            print("{:.2f}".format(line[i]), end="\t")
+        print("{:.2f}".format(line[-1]))
